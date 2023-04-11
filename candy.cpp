e1dab5ad4abd27f9d5ee23e31a0a2759a23a51f7
Christian Green <122922258+chgr4354@users.noreply.github.com>
2023-04-11 11:48:15 -0600

    Update candy.cpp

--- candy.cpp
+++ candy.cpp
@@ -2,12 +2,16 @@
 
 using namespace std;
 
-candy::candy(char new_color = '-', int new_row, int new_column) { //initialize a new candy object with default color '-'
+candy::candy(char new_color = '-', int new_row = 0, int new_column = 0) { //initialize a new candy object with default values
     color = new_color;
     if(new_color == '⭐' || new_color == '🎁')
         isSpecial = true;
     else
         isSpecial = false;
+    if(new_color == '🟦'|| new_color == '🟩' || new_color == '🟥' || new_color == '⬜' || new_color == '-' || new_color == isSpecial)
+        isModifiable = true;
+    else
+        isModifiable = false;
     rowLocation = new_row;
     columnLocation = new_column;
 }
@@ -16,13 +20,17 @@ candy::candy(char new_color = '-', int new_row, int new_column) { //initialize a
 char candy::getColor() {
     return color;
 }
-
 int candy::getRow() {
     return rowLocation;
 }
-
 int candy::getColumn() {
     return columnLocation;
+}
+bool candy::isSpecialCandy() { //check if candy is a special candy
+    return isSpecial;
+}
+bool candy::isModifiableCandy() { //check if candy is a "candy" or a coordinate emoji
+    return isModifiable;
 }
 
 //setters
@@ -35,12 +43,8 @@ void candy::setRow(int new_row) {
 void candy::setColumn(int new_column) {
     columnLocation = new_column;
 }
-
-//other
-bool candy::isSpecialCandy() {
-    if(isSpecial) {
-        return true;
-    }
-    else
-        return false;
+void candy::setAll(char new_color, int new_row, int new_column) {
+    color = new_color;
+    rowLocation = new_row;
+    columnLocation = new_column;
 }
