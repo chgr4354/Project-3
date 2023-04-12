4c9ffca8d5419fe0c4740ad6160fd2e82fd91559
Christian Green <122922258+chgr4354@users.noreply.github.com>
2023-04-12 13:26:25 -0600

    Update candyCrush.h

--- candyCrush.h
+++ candyCrush.h
@@ -1,30 +1,45 @@
-#ifndef CANDY_H
-#define CANDY_H
+#ifndef CANDYCRUSH_H
+#define CANDYCRUSH_H
 
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include "candy.h"
 
 using namespace std;
 
-class candy {
+class candyCrush {
+
     private:
-        string color;
-        bool isSpecial;
-        bool isModifiable;
-        int rowLocation;
-        int columnLocation;
+        static const int rows = 11;      //number of rows in level
+        static const int columns = 11;   //number of columns in level
+        int score;
+
+        candy board[rows][columns];
+
     public:
-        candy(); //not yet implemented in candy.cpp
-        candy(string color, int row, int column);
-        string getColor();
-        int getRow();
-        int getColumn();
-        void setColor(string color);
-        void setRow(int row);
-        void setColumn(int column);
-        void setAll(string color, int row, int column);
-        void move(char direction);
-        bool isSpecialCandy();
-        bool isModifiableCandy();
+        candyCrush(string level);
+
+        void initializeBoard(string level); //initializes board by reading from file
+        void resetBoard();
+        void displayBoard();      //displays level
+        bool swap(int row1, int col1, int row2, int col2); //swaps elements
+        void generateCandy(int row, int col);   //inserts new color into position in level
+        void removeMatches(int row, int col);
+
+        int getRows();
+        int getColumns();
+        int getScore();
+
+        void setRows(int rows);
+        void setColumns(int col);
+        void setScore(int score);
+
+        bool isOnBoard(int row, int col);
+        bool isSameColor(int row, int col, int row2, int col2);
+        bool isStar(int row, int col);
+        bool isGift(int row, int col);
+
 
 };
 #endif
