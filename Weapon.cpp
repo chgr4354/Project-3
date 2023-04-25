e75aab0890aa1934303f62ef5addf60f265a09da
Christian Green <122922258+chgr4354@users.noreply.github.com>
2023-04-24 22:07:08 -0600

    Update Weapon.cpp

--- Weapon.cpp
+++ Weapon.cpp
@@ -2,19 +2,26 @@
 
 using namespace std;
 
+//constructors
 Weapon::Weapon() {
-    name = "";
-    damage = 0;
-    story = "";
+    name = "butter knife";
+    damage = 1;
+    cost = 0;
+    story = "The butter knife you grabbed from your favorite Italian place before you began your journey. They'll never notice.";
 }
-Weapon::Weapon(string new_name, int new_damage, string new_story) {
+Weapon::Weapon(string new_name, int new_damage, int new_cost, string new_story) {
     name = new_name;
     damage = new_damage;
+    cost = new_cost;
     story = new_story;
 }
 
+//getters
 int Weapon::getDamage() {
     return damage;
+}
+int Weapon::getCost() {
+    return cost;
 }
 string Weapon::getName() {
     return name;
@@ -23,8 +30,12 @@ string Weapon::getStory() {
     return story;
 }
 
+//setters
 void Weapon::setDamage(int new_damage) {
     damage = new_damage;
+}
+void Weapon::setCost(int new_cost) {
+    cost = new_cost;
 }
 void Weapon::setName(string new_name) {
     name = new_name;
