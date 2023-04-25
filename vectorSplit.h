d9b29e4aac1505a69b264c3d96b0f2c552351bc3
Christian Green <122922258+chgr4354@users.noreply.github.com>
2023-04-24 22:11:53 -0600

    Update vectorSplit.h

--- vectorSplit.h
+++ vectorSplit.h
@@ -1,3 +1,6 @@
+#ifndef VECTORSPLIT_H
+#define VECTORSPLIT_H
+
 #include <iostream>
 #include <vector>
 #include <fstream>
@@ -5,28 +8,5 @@
 
 using namespace std;
 
-void vectorSplit(string filename, const char delimiter, vector<string>& data) {
-    ifstream file(filename);
-
-    if(!file.fail()) {
-        string line;
-        while(getline(file, line)) {
-            string value;
-            for(int i = 0; i < line.size(); i++) {
-                char ch = line[i];
-                if(ch == delimiter) {
-                    data.push_back(value);
-                    value.clear();
-                }
-                else
-                    value.push_back(ch);
-            }
-            if (!value.empty()) {
-                data.push_back(value);
-            }
-        }
-    }
-    else
-        cout << "vectorSplit - failed to open \"" << filename << "\"" << endl;
-
-}
+void vectorSplit(string filename, const char delimiter, vector<string>& data);
+#endif
