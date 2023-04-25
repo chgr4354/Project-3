4d671224a10ef0b90a83e260bad9d6835b5349bd
Christian Green <122922258+chgr4354@users.noreply.github.com>
2023-04-24 22:09:31 -0600

    Update candyCrush.h

--- candyCrush.h
+++ candyCrush.h
@@ -18,23 +18,29 @@ class candyCrush {
         candy board[rows][columns];
 
     public:
+        candyCrush();
         candyCrush(string level);
 
+        //other
         void initializeBoard(string level); //initializes board by reading from file
         void resetBoard();
         void displayBoard();      //displays level
         bool swap(int row1, int col1, int row2, int col2); //swaps elements
-        void generateCandy(int row, int col);   //inserts new color into position in level
+        void generateCandy();   //inserts new color into position in level
         void removeMatches(string color, int row, int col);
 
+        //getters
         int getRows();
         int getColumns();
         int getScore();
+        candy getCandy(int row, int col);
 
+        //setters
         void setRows(int rows);
         void setColumns(int col);
         void setScore(int score);
 
+        //bools
         bool isOnBoard(int row, int col);
         bool isSameColor(int row, int col, int row2, int col2);
         bool isStar(int row, int col);
