60fd6e8bfb16085fe6a73dec7b1c3131be943a4f
Christian Green <122922258+chgr4354@users.noreply.github.com>
2023-04-30 13:54:43 -0600

    Update Monster.cpp

--- Monster.cpp
+++ Monster.cpp
@@ -4,41 +4,42 @@
 
 using namespace std;
 
-Monster::Monster() {
+Monster::Monster() { //default constructor
     name = "Lv. 1 Mafia Boss";
     damage = 0;
     health = 0;
 }
 
-Monster::Monster(string filename) {
+Monster::Monster(string filename) { //paramaterized constructor
     vector<string> data;
     vectorSplit(filename, '|', data);
 
     int numMonsters = data.size() / 3; // calculate number of monsters in the file
     int randNum = rand() % numMonsters; // generate random number between 0 and numMonsters-1
 
+    //randomly pick a monster from the file
     name = data.at(randNum*3);
     health = stoi(data.at(randNum*3+1));
     damage = stoi(data.at(randNum*3+2));
 }
 
 
-void Monster::setName(string new_name) {
+void Monster::setName(string new_name) { //set name
     name = new_name;
 }
-void Monster::setHealth(int new_health) {
+void Monster::setHealth(int new_health) { //set health
     health = new_health;
 }
-void Monster::setDamage(int new_damage) {
+void Monster::setDamage(int new_damage) { //set damage
     damage = new_damage;
 }
 
-string Monster::getName() {
+string Monster::getName() { //get name
     return name;
 }
-int Monster::getHealth() {
+int Monster::getHealth() { //get health
     return health;
 }
-int Monster::getDamage() {
+int Monster::getDamage() { //get damage
     return damage;
 }
