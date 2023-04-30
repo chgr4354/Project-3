25bef62f3ec5ac98d778e7c10a41b88e5a2c011a
Christian Green <122922258+chgr4354@users.noreply.github.com>
2023-04-30 13:50:54 -0600

    Update candy.cpp

--- candy.cpp
+++ candy.cpp
@@ -2,35 +2,35 @@
 
 using namespace std;
 
-candy::candy() {
+candy::candy() { //default candy constructor
     color = "-";
     rowLocation = 0;
     columnLocation = 0;
 }
 
-candy::candy(string new_color = "-", int new_row = 0, int new_column = 0) { //initialize a new candy object
-    color = new_color;
-    if(new_color == "⭐" || new_color == "🎁")
+candy::candy(string new_color = "-", int new_row = 0, int new_column = 0) { //parameterized candy constructor
+    color = new_color; //set color
+    if(new_color == "⭐" || new_color == "🎁") //set isSpecial condition
         isSpecial = true;
     else
         isSpecial = false;
+    //set isModifiable condition
     if(new_color == "🟦"|| new_color == "🟨" || new_color == "🟥" || new_color == "⬜" || new_color == "-" || new_color == "⭐" || new_color == "🎁")
         isModifiable = true;
     else
         isModifiable = false;
-    rowLocation = new_row;
-    columnLocation = new_column;
+    rowLocation = new_row; //set row
+    columnLocation = new_column; //set column
 }
-
 
 //getters
-string candy::getColor() {
+string candy::getColor() { //get candy color
     return color;
 }
-int candy::getRow() {
+int candy::getRow() { //get candy row
     return rowLocation;
 }
-int candy::getColumn() {
+int candy::getColumn() { //get candy column
     return columnLocation;
 }
 bool candy::isSpecialCandy() { //check if candy is a special candy
@@ -41,28 +41,29 @@ bool candy::isModifiableCandy() { //check if candy is a "candy" or a coordinate 
 }
 
 //setters
-void candy::setColor(string new_color) {
-    if(new_color == "⭐" || new_color == "🎁")
+void candy::setColor(string new_color) { //set new color of candy
+    if(new_color == "⭐" || new_color == "🎁") //change isSpecial condition
         isSpecial = true;
-    color = new_color;
+    color = new_color; //change color
+    //change isModifiable condition
     if(new_color == "🟦"|| new_color == "🟨" || new_color == "🟥" || new_color == "⬜" || new_color == "-" || new_color == "⭐" || new_color == "🎁")
         isModifiable = true;
     else
         isModifiable = false;
 }
-void candy::setRow(int new_row) {
+void candy::setRow(int new_row) { //set row
     rowLocation = new_row;
 }
-void candy::setColumn(int new_column) {
+void candy::setColumn(int new_column) { //set column
     columnLocation = new_column;
 }
-void candy::setAll(string new_color, int new_row, int new_column) {
+void candy::setAll(string new_color, int new_row, int new_column) { //change everything about a candy
     color = new_color;
     rowLocation = new_row;
     columnLocation = new_column;
 }
 
-/* other, may not be necessary
+/**other, not necessary
 //***DOES NOT CHECK IF LOCATION isOnBoard()****
 void candy::move(char direction) { //moves candy to new location (w,a,s,d)
     switch (direction) {
