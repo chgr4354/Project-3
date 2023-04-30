5b09a8f876e33997722197e97246db29d8be93ea
Christian Green <122922258+chgr4354@users.noreply.github.com>
2023-04-30 13:56:07 -0600

    Update vectorSplit.cpp

--- vectorSplit.cpp
+++ vectorSplit.cpp
@@ -6,28 +6,42 @@
 #include <string>
 using namespace std;
 
-void vectorSplit(string filename, const char delimiter, vector<string>& data) {
-    ifstream file(filename);
+/*
+ * Algorithm: Splits a file into a vector of strings by locating delimiters in the file.
+ * Open the file using an ifstream object
+ * Read through the file one line at a time using a while loop
+ * For each line, create a string variable to store the current value.
+ * Iterate through the characters in the line, and if the current character is the delimiter, add the current value to the vector and clear the value variable.
+ * If the value variable is not empty, add it to the vector.
+ * Close the file.
+ * parameters: filename (string), delimeter (const char), data (vector<string>)
+ * returns: nothing (void)
+*/
 
-    if(!file.fail()) {
+
+void vectorSplit(string filename, const char delimiter, vector<string>& data) {
+    ifstream file(filename); //create ifstream object
+
+    if(!file.fail()) { //check that file can be opened
         string line;
-        while(getline(file, line)) {
+        while(getline(file, line)) { //while there are new lines in the file
             string value;
-            for(int i = 0; i < line.size(); i++) {
+            for(int i = 0; i < line.size(); i++) { //loop through the line of the file
                 char ch = line[i];
-                if(ch == delimiter) {
-                    data.push_back(value);
-                    value.clear();
+                if(ch == delimiter) { //check for delimieter
+                    data.push_back(value); //add section to vector
+                    value.clear(); //reset value
                 }
                 else
-                    value.push_back(ch);
+                    value.push_back(ch); //
             }
-            if (!value.empty()) {
+            if (!value.empty()) { //if value has been assigned, add value to data vector
                 data.push_back(value);
             }
         }
     }
-    else
+    
+    else //file could not be opened
         cout << "vectorSplit - failed to open \"" << filename << "\"" << endl;
-
+    file.close(); //close file
 }
