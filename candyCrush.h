d9541f6e91dea3cb48028ecff7b399276816d7ba
Christian Green <122922258+chgr4354@users.noreply.github.com>
2023-04-30 13:52:48 -0600

    Update candyCrush.h

--- candyCrush.h
+++ candyCrush.h
@@ -11,42 +11,39 @@ using namespace std;
 class candyCrush {
 
     private:
-        static const int rows = 11;      //number of rows in level
-        static const int columns = 11;   //number of columns in level
-        int score = 0;
-
-        candy board[rows][columns];
+        static const int rows = 11;     //number of rows in level
+        static const int columns = 11;  //number of columns in level
+        int score = 0;                  //score for level
+        candy board[rows][columns];     //array of candy objects to represent the board
 
     public:
-        candyCrush();
-        candyCrush(string level);
+        candyCrush();               //deafult constructor
+        candyCrush(string level);   //parameterized constructor takes in filename
 
         //other
-        void initializeBoard(string level); //initializes board by reading from file
-        void resetBoard();
-        void displayBoard();      //displays level
-        bool swap(int row1, int col1, int row2, int col2); //swaps elements
-        void generateCandy();   //inserts new color into position in level
-        void removeMatches(string color, int row, int col);
+        void initializeBoard(string level);                 //initializes board by reading from file
+        void resetBoard();                                  //resets board
+        void displayBoard();                                //displays level
+        bool swap(int row1, int col1, int row2, int col2);  //swaps elements
+        void generateCandy();                               //inserts new color into position in level
+        void removeMatches(string color, int row, int col); //remove candies of similar color
 
         //getters
-        int getRows();
-        int getColumns();
-        int getScore();
-        candy getCandy(int row, int col);
+        int getRows();                      //get rows
+        int getColumns();                   //get columns
+        int getScore();                     //get level score
+        candy getCandy(int row, int col);   //get a candy at a certain row,col
 
         //setters
-        void setRows(int rows);
-        void setColumns(int col);
-        void setScore(int score);
+        void setRows(int rows);     //set rows
+        void setColumns(int col);   //set columns
+        void setScore(int score);   //set level score
 
         //bools
-        bool isOnBoard(int row, int col);
-        bool isSameColor(int row, int col, int row2, int col2);
-        bool isStar(int row, int col);
-        bool isGift(int row, int col);
-
-        //for future, may want isColor() function for easier testing
+        bool isOnBoard(int row, int col);                       //check if candy is on board
+        bool isSameColor(int row, int col, int row2, int col2); //check if two candies are the same color
+        bool isStar(int row, int col);                          //check if candy is color star
+        bool isGift(int row, int col);                          //check if candy is color gift
 
 };
 #endif
