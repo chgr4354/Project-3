0b313040d745114483e07eafe2a9c0c72ff80c63
Christian Green <122922258+chgr4354@users.noreply.github.com>
2023-04-30 16:26:17 -0600

    Update foodDriver.cpp

--- foodDriver.cpp
+++ foodDriver.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 #include "Food.h"
 
 int main()
@@ -10,12 +11,19 @@ int main()
     cout << "Name: " << food1.getName() << endl;
     cout << "Cost: " << food1.getCost() << endl;
     cout << "Recover: " << food1.getRecover() << endl;
+    assert(food1.getName() == "");
+    assert(food1.getCost() == 0);
+    assert(food1.getRecover() == 0);
 
     //Paramterized Contructor
-    Food food2 = Food("STINKY LETTUCE(LOW CARB)", 100, 2000);
+    Food food2 = Food("LETTUCE", 100, 2000);
 
     //Testing Member Functions
     cout << "Name: " << food2.getName() << endl;
     cout << "Cost: " << food2.getCost() << endl;
     cout << "Recover: " << food2.getRecover() << endl;
+    assert(food2.getName() == "LETTUCE");
+    assert(food2.getCost() == 2000);
+    assert(food2.getRecover() == 100);
+    
 }
