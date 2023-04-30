cd6405b2d213dcbf4921274e55745615680600e4
Christian Green <122922258+chgr4354@users.noreply.github.com>
2023-04-30 13:54:25 -0600

    Update Monster.h

--- Monster.h
+++ Monster.h
@@ -8,20 +8,20 @@ using namespace std;
 class Monster
 {
     private:
-        string name;
-        int health; //Monster Health
-        int damage; //How much damage the monsters do
+        string name; //Monster name
+        int health;  //Monster Health
+        int damage;  //How much damage the monsters do
 
     public:
+        Monster();                  //Default Constructor
+        Monster(string filename);   //parameterized constructor
 
-        Monster(); //Default Constructor
-        Monster(string filename);
+        void setName(string set_name);  //set name of monster
+        void setHealth(int health);     //Sets The Health of the Monster
+        void setDamage(int damage);     //set damage for monster
 
-        void setName(string set_name);
-        void setHealth(int health); //Sets The Health of the Monster
-        void setDamage(int damage);
-
-        int getDamage();
-        int getHealth(); //Gets Health of Monster
-        string getName();
+        int getDamage();  //get damage of monster
+        int getHealth();  //Gets Health of Monster
+        string getName(); //get name of monster
+        
 };
